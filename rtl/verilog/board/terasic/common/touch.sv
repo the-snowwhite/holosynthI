28a72304295fd8f7269a3ace8ddc296f13ad6d6d
mib <mib@hololab.localdomain>
2012-02-26 06:53:10 +0100

    got hit_x + hit_y working in touch.sv
    first step in working touchpanel parameter editing and load save functionality

--- rtl/verilog/board/terasic/common/touch.sv
+++ rtl/verilog/board/terasic/common/touch.sv
@@ -85,7 +85,65 @@ wire confirm = ((hit_y >=94 && hit_y <=101) && // write line
 wire [7:0] slide_scale = (((x - 40)<<7)/180);
 
 	reg [7:0] edit_chr;
+	reg [2:0] penirq_cnt;
+	reg penirq_cnt_en, penirq_n_r, transmit_en_r;
+	
+	wire sample_hit = (penirq_cnt >= 3'd3);
+	
+	reg sample_hit_r, transmit_en_dly;
+	
+	always @(posedge sys_clk) begin
+		sample_hit_r <= sample_hit;
+		penirq_n_r <= penirq_n;
+		transmit_en_r <= transmit_en;
+		transmit_en_dly <= transmit_en_r;
+	end
+	
+	always @(negedge transmit_en_r)begin
+		rel_x <= x;
+		rel_y <= y;
+	end
 
+	always @(negedge iRST_n or posedge sample_hit_r)begin
+		if (!iRST_n)begin
+			hit_x <= 0; hit_y <= 0;
+		end else begin
+			if(sample_hit_r) begin
+				hit_x <= x; hit_y <= y;		
+			end
+		end
+	end
+	
+	always @(negedge iRST_n or posedge transmit_en_r or posedge sample_hit_r or negedge transmit_en_dly) begin
+		if (!iRST_n)begin
+			penirq_cnt_en <= 1'b0;
+		end else begin 
+			if (transmit_en_r)begin
+				if (!sample_hit_r && !transmit_en_dly)begin
+					penirq_cnt_en <= 1'b1;
+				end
+				else if (sample_hit_r && transmit_en_dly)begin
+					penirq_cnt_en <= 1'b0;
+				end
+			end
+		end
+	end	
+
+	always @(negedge iRST_n or negedge penirq_n_r)begin
+		if (!iRST_n)begin
+			penirq_cnt <= 3'd0;
+		end else begin 
+			if (!penirq_n_r) begin 
+				if(penirq_cnt_en)begin
+					penirq_cnt <= penirq_cnt + 1;
+				end
+				else begin
+					penirq_cnt <= 3'd0;
+				end
+			end
+		end
+	end	
+	
 	always @(posedge sys_clk)begin
 		case(lne)
 			1: if(chr_3 <=7)disp_val <= chr_3;
@@ -107,85 +165,80 @@ reg [7:0]t_cnt;
 reg [2:0]cont_cnt;
 reg cont_max_dly1,cont_max_dly2;
 
-reg transmit_en_dly1;
-
-always@(posedge sys_clk) transmit_en_dly1 <= transmit_en;
-
-	always @(negedge penirq_n or negedge transmit_en)begin
-		if(!transmit_en)begin t_cnt <= 0;end 
-		else if(t_cnt <=7) t_cnt <= t_cnt +1;
+	always @(negedge iRST_n or negedge penirq_n or negedge transmit_en)begin
+		if (!iRST_n)begin
+			t_cnt <= 0;
+		end else begin
+			if(!transmit_en)begin t_cnt <= 0;end 
+			else if(t_cnt <=7) t_cnt <= t_cnt +1;
+		end
 	end
-	always @(negedge penirq_n or negedge transmit_en)begin
-		if(!transmit_en)begin cont_cnt <= 0;end 
-		else if (t_cnt_max) cont_cnt <= cont_cnt +1;
+	always @(negedge iRST_n or negedge penirq_n or negedge transmit_en)begin
+		if (!iRST_n)begin
+			cont_cnt <= 0;
+		end else begin
+			if(!transmit_en)begin cont_cnt <= 0;end 
+			else if (t_cnt_max) cont_cnt <= cont_cnt +1;
+		end
 	end
 
 	wire t_cnt_max = (t_cnt == 8) ? 1'b1 : 1'b0;
 
 	always @(negedge iRST_n or negedge penirq_n or negedge transmit_en)begin
-	if (!iRST_n)begin
-		write_slide <= 1'b0;
-		N_sound_nr <= 8'h00;
-	end
-	else begin 
-		if (!transmit_en)begin
-			if (plus_hit || minus_hit)begin 
-				write_slide <= ((~N_save_sig_1 & ~N_load_sig_1) & transmit_en_dly1);
-//				if ((N_save_sig_1 || N_load_sig_1)&& (slide_scale < 128) )begin
-				if (N_save_sig_1 || N_load_sig_1) begin
-					N_sound_nr <= slide_val;
-				end
-			end
+		if (!iRST_n)begin
+			write_slide <= 1'b0;
+			N_sound_nr <= 8'h00;
 		end
-		else begin
-//			write_slide <= 1'b0;
-			if (t_cnt == 7)begin
-				hit_x <= x; hit_y <= y;		
+		else begin 
+			if (!transmit_en)begin
+				if (plus_hit || minus_hit)begin 
+					write_slide <= ((~N_save_sig_1 & ~N_load_sig_1) & transmit_en_dly);
+					if (N_save_sig_1 || N_load_sig_1) begin
+						N_sound_nr <= slide_val;
+					end
+				end
 			end
-			if (cont_cnt == 1)begin
-				if ((!N_save_sig_1 && !N_save_sig) && (!N_load_sig_1 && !N_load_sig)
+			else begin
+				if (cont_cnt == 1)begin
+					if ((!N_save_sig_1 && !N_save_sig) && (!N_load_sig_1 && !N_load_sig)
 					&& (in_textarea && no_drag))begin
-					chr_3 <= cur_h_x;
-					lne <= cur_h_y;
+						chr_3 <= cur_h_x;
+						lne <= cur_h_y;
+					end
+					else if(N_save_sig_1 && !N_save_sig)begin
+						chr_3 <= 4'd11;
+						lne <= 4'd11;
+					end
+					else if(N_load_sig_1 && !N_load_sig)begin
+						chr_3 <= 4'd11;
+						lne <= 4'd09;
+					end
+					else if (N_save_sig || N_load_sig)begin
+						chr_3 <= cur_h_x;
+						lne <= cur_h_y;
+					end		
 				end
-				else if(N_save_sig_1 && !N_save_sig)begin
-					chr_3 <= 4'd11;
-					lne <= 4'd11;
-				end
-				else if(N_load_sig_1 && !N_load_sig)begin
-					chr_3 <= 4'd11;
-					lne <= 4'd09;
-				end
-				else if (N_save_sig || N_load_sig)begin
-					chr_3 <= cur_h_x;
-					lne <= cur_h_y;
-				end		
-			end
-			if (cont_cnt == 2)begin
-				if(N_save_sig_1 || N_load_sig_1)
-					slide_val <= N_sound_nr;
-				else 
-					slide_val <= edit_chr;
-			end
-			if (cont_cnt == 3)begin
-				if (slide_bar_hit && (slide_scale < 128)) slide_val <= slide_scale;
-				else if (minus_hit && (slide_val != 0))
-	//			else if (minus_hit)
-					slide_val <= slide_val - 1;
-				else if (plus_hit && slide_val < 127)
-	//			else if (plus_hit)
-					slide_val <= slide_val + 1;		
-			end
-			if(cont_cnt == 4)begin
-				if (slide_bar_hit)begin
-//					write_slide <= (~N_save_sig_1 | ~N_load_sig_1);
+				if (cont_cnt == 2)begin
 					if(N_save_sig_1 || N_load_sig_1)
-						write_slide <= 1'b1;							
-//						N_sound_nr <= slide_val;
+						slide_val <= N_sound_nr;
+					else 
+						slide_val <= edit_chr;
 				end
-			end
-			if(cont_cnt == 5)begin
-				write_slide <= 1'b0;
+				if (cont_cnt == 3)begin
+					if (slide_bar_hit && (slide_scale < 128)) slide_val <= slide_scale;
+					else if (minus_hit && (slide_val != 0))
+						slide_val <= slide_val - 1;
+					else if (plus_hit && slide_val < 127)
+						slide_val <= slide_val + 1;		
+				end
+				if(cont_cnt == 4)begin
+					if (slide_bar_hit)begin
+						if(N_save_sig_1 || N_load_sig_1)
+							write_slide <= 1'b1;							
+					end
+				end
+				if(cont_cnt == 5)begin
+					write_slide <= 1'b0;
 				end
 			end
 		end
@@ -207,11 +260,6 @@ always@(posedge sys_clk) transmit_en_dly1 <= transmit_en;
 		if(cancel || N_adr_9) begin N_load_sig <= 1'b0; N_load_sig_1 <= 1'b0;end
 		else if(load_pressed) N_load_sig_1 <= 1'b1;
 		else if(confirm) N_load_sig <= N_load_sig_1;
-	end
-
-	always @(negedge transmit_en)begin
-		rel_x <= x;
-		rel_y <= y;
 	end
 
 endmodule
